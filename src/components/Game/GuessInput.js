13c9e377da146bfcd3fbf4459f2f0160542d137b
Victor Lacorte <victor.lacorte@useorigin.com>
2023-10-01 10:36:54 -0300

    Complete exercise 1

---
+++ src/components/Game/GuessInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  function handleChange(e) {
+    setGuess(String(e.target.value).toUpperCase());
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        pattern="\w{5}"
+        title="5 letter word"
+        required={true}
+        value={guess}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
